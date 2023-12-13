3bae604a470015dffdfbeeb838a67769a1af638f
HussainFathy <76268960+HussainFathy@users.noreply.github.com>
2023-12-13 21:57:07 +0200

    Add files via upload

---
+++ pages/posts/post-1.js
@@ -0,0 +1,12 @@
+// pages/posts/post-1.js
+
+const Post1 = () => {
+  return (
+    <div>
+      <h1>Post 1: Introduction to Cybersecurity</h1>
+      <p>This is an introduction to the exciting world of cybersecurity.</p>
+    </div>
+  );
+};
+
+export default Post1;
