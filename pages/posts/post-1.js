8fd461d2b985404a8ed79994821ce6012cb1baa3
HussainFathy <76268960+HussainFathy@users.noreply.github.com>
2023-12-13 22:22:44 +0200

    Update post-1.js

--- pages/posts/post-1.js
+++ pages/posts/post-1.js
@@ -1,12 +1,51 @@
-// pages/posts/post-1.js
+// pages/posts/[slug].js
 
-const Post1 = () => {
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const Post = ({ post }) => {
   return (
     <div>
-      <h1>Post 1: Introduction to Cybersecurity</h1>
-      <p>This is an introduction to the exciting world of cybersecurity.</p>
+      <h1>{post.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: post.content }} />
     </div>
   );
 };
 
-export default Post1;
+export async function getStaticPaths() {
+  const postsDirectory = path.join(process.cwd(), 'pages/posts');
+  const filenames = fs.readdirSync(postsDirectory);
+
+  const paths = filenames.map((filename) => ({
+    params: {
+      slug: filename.replace(/\.md$/, ''),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+  const filePath = path.join(process.cwd(), 'pages/posts', `${slug}.md`);
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+
+  // Extract metadata (e.g., title) from the file content
+  const { data, content } = matter(fileContent);
+
+  return {
+    props: {
+      post: {
+        id: data.id,
+        title: data.title,
+        content: content,
+      },
+    },
+  };
+}
+
+export default Post;
