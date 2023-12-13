ecffe7e7517c82f4d50629cf2e1157c914cef583
HussainFathy <76268960+HussainFathy@users.noreply.github.com>
2023-12-14 00:10:34 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <img src="/sph2.png" alt="Sphinx Logo" className={styles.logo} />
       </footer>
     </>
   )
