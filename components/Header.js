d2c45121d31e6688650fdfecbc01ed8c3292726f
HussainFathy <76268960+HussainFathy@users.noreply.github.com>
2023-12-13 21:35:20 +0200

    test1Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,48 @@
-export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
-}
+// components/Header.js
+
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/whoami">
+              <a>Whoami</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/cve">
+              <a>CVE</a>
+            </Link>
+          </li>
+        </ul>
+        <ul className="external-links">
+          <li>
+            <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+              Linkedin
+            </a>
+          </li>
+          <li>
+            <a href="mailto:your@email.com">
+              Email
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
