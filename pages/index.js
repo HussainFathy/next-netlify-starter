3db92ef1796ff6c99ece04795a7af6d993940670
HussainFathy <76268960+HussainFathy@users.noreply.github.com>
2023-12-13 21:46:35 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,21 @@ import Header from '@components/Header';
 import Footer from '@components/Footer';
 
 const Home = () => {
+  // Sample blog posts data
+  const blogPosts = [
+    {
+      id: 1,
+      title: 'Introduction to Cybersecurity',
+      content: 'This is an introduction to the exciting world of cybersecurity.',
+    },
+    {
+      id: 2,
+      title: 'Securing Your Online Presence',
+      content: 'Learn essential tips to secure your online accounts and data.',
+    },
+    // Add more blog posts as needed
+  ];
+
   return (
     <div className="container">
       <Head>
@@ -15,7 +30,17 @@ const Home = () => {
       <Header />
 
       <main>
-        {/* Your blog content goes here */}
+        <h1>Welcome to Your Cybersecurity Blog!</h1>
+        
+        {/* Display a list of blog posts */}
+        <ul>
+          {blogPosts.map((post) => (
+            <li key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.content}</p>
+            </li>
+          ))}
+        </ul>
       </main>
 
       <Footer />
