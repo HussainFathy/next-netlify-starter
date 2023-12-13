0bca0015c894b9415a536aa6a3fe89fce1f526fc
HussainFathy <76268960+HussainFathy@users.noreply.github.com>
2023-12-13 21:28:17 +0200

    test1 index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,41 @@
+// Import necessary components and modules
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+
+// Sample blog posts data
+const blogPosts = [
+  {
+    id: 1,
+    title: 'My First Blog Post',
+    content: 'This is the content of my first blog post. Welcome to my blog!',
+  },
+  {
+    id: 2,
+    title: 'Another Blog Post',
+    content: 'Here is another interesting blog post. Hope you enjoy reading!',
+  },
+  // Add more blog posts as needed
+];
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Your Personal Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Your Personal Blog" />
+
+        {/* Display a list of blog posts */}
+        {blogPosts.map((post) => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+          </div>
+        ))}
       </main>
 
       <Footer />
