7971f8b07f588c2b1a4e455d4b35f0cf9cad2ed4
HussainFathy <76268960+HussainFathy@users.noreply.github.com>
2023-12-14 01:01:28 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -5,8 +5,8 @@ import styles from './Header.module.css';
 
 const Header = () => {
   return (
-    <header>
-      <nav className={styles.headerNav}>
+    <header style="width: 100%;">>
+      <nav className={styles.headerNav} style="height: 40px;">
         <div className={styles.responsiveDivTop}>
           <Link href="/">
             <a><b>Home</b></a>
